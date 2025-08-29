f5fc5bf8f44075ff6c32cbb9d7d7083ba5f5f008
Oscar Sanchez <oscar.sanchezmoli.07@gmail.com>
2025-08-29 08:22:00 -0500

    chore: se inica el proyecto en blanco

---
+++ src/main/java/com/seti/pruebaTecnicaSeti/PruebaTecnicaSetiApplication.java
@@ -0,0 +1,13 @@
+package com.seti.pruebaTecnicaSeti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSetiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaSetiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seti/pruebaTecnicaSeti/PruebaTecnicaSetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seti.pruebaTecnicaSeti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
